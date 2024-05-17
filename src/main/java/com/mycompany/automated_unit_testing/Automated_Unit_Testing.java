f15f65e83519c2aac16078df8631d298e78ed835
5wazi <161421767+5wazi@users.noreply.github.com>
2024-05-17 12:02:24 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/automated_unit_testing/Automated_Unit_Testing.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automated_unit_testing;
+
+/**
+ *
+ * @author samke
+ */
+public class Automated_Unit_Testing {
+
+    public static void main(String[] args) {
+        
+        /*Automated Unit Testing --> GitHun actions
+        -Modify the pom.xml file
+        - Create a workflow called JavaTest Using GitHub Actions
+        - The JDK Version included in the workflow will depend on the 
+          version you used to create the program
+        */
+    }
+}
